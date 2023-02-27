34211b0c3551e8b67b7e8fafb8955b4fce74ffc2
Lucas Pimentel Lellis <lucaslellis@gmail.com>
2023-02-27 18:24:21 +0000

    Melhorias de sintaxe

--- gen_logrotate_config.sh
+++ gen_logrotate_config.sh
@@ -16,9 +16,9 @@ SCRIPT_VERSAO_BANCO="${DIR_BASE}/../obter_versao_banco.sql"
 OUT="${DIR_BASE}/oracle_asm_logrotate.conf"
 rm -f $OUT
 if [ -n "$(ps -ef | grep "asm_[p]mon" | grep -v grep)" ]; then
-    (for I in `\ps -ef | egrep "(asm)_[p]mon_" | awk '{print $NF}' | sed 's/.*pmon_//'`
+    (for I in $(\ps -ef | egrep "(asm)_[p]mon_" | awk '{print $NF}' | sed 's/.*pmon_//')
     do
-            DB=`echo ${I} | sed s/'[1-9]$//'`
+            DB=$(echo ${I} | sed s/'[1-9]$//')
 
             . oraenv <<< ${I}       > /dev/null 2>&1
 
@@ -31,8 +31,8 @@ END_SQL
 
     done) | grep -v "^$" | awk -F, '{ printf $1 " "}' >> $OUT
     printf "\n" >> ${OUT}
-    host_nm=`hostname | cut -d'.' -f1`
     # only works with root access
+    # host_nm=$(hostname | cut -d'.' -f1)
     # if [ -e "$ORACLE_HOME/log/$host_nm/alert${host_nm}.log" ]; then
     #     printf "$ORACLE_HOME/log/$host_nm/alert${host_nm}.log\n" >> $OUT
     # fi
@@ -46,7 +46,7 @@ END_SQL
         compress
         copytruncate
         nodelaycompress
-        create 0640 `whoami` dba
+        create 0640 $(whoami) dba
         notifempty
         dateext
 }
@@ -55,14 +55,14 @@ fi
 
 OUT="${DIR_BASE}/oracle_rdbms_logrotate.conf"
 rm -f $OUT
-(for I in `\ps -ef | egrep "(ora)_[p]mon_" | awk '{print $NF}' | sed 's/.*pmon_//'`
+(for I in $(\ps -ef | egrep "(ora)_[p]mon_" | awk '{print $NF}' | sed 's/.*pmon_//')
 do
-        DB=`echo ${I}`
+        DB=$(echo ${I})
 
         . oraenv <<< ${DB}      > /dev/null 2>&1
     export ORACLE_SID=${I}
 
-    versao_banco=`\sqlplus -S "/ as sysdba" @${SCRIPT_VERSAO_BANCO}`
+    versao_banco=$(\sqlplus -S "/ as sysdba" @${SCRIPT_VERSAO_BANCO})
     if [[ "$versao_banco" -lt "11" ]]; then
         sqlplus -S "/ as sysdba" << END_SQL
         set lines 200   ;
@@ -87,7 +87,7 @@ cat << !                        >> ${OUT}
         compress
         copytruncate
         nodelaycompress
-        create 0640 `whoami` dba
+        create 0640 $(whoami) dba
         notifempty
         dateext
 }
@@ -97,12 +97,12 @@ cat << !                        >> ${OUT}
 #
 
 # We need to have the CRS env to check the listeners
-. oraenv <<< `\ps -ef | grep "asm_[p]mon" | grep -v grep | sed s'/^.*_//g'` > /dev/null 2>&1
+. oraenv <<< $(\ps -ef | grep "asm_[p]mon" | grep -v grep | sed s'/^.*_//g') > /dev/null 2>&1
 OUT="${DIR_BASE}/oracle_listener_logrotate.conf"
 rm -f $OUT
-for L in `\ps -ef | grep tnslsnr | grep -v grep | sed -r s'/tnslsnr \b([A-Za-z0-9_-]+)\b -.*$/tnslsnr \1/g' | grep -v sed | awk '{print $NF}'`
+for L in $(\ps -ef | grep tnslsnr | grep -v grep | sed -r s'/tnslsnr \b([A-Za-z0-9_-]+)\b -.*$/tnslsnr \1/g' | grep -v sed | awk '{print $NF}')
 do
-        LSRN_LOG=`lsnrctl status ${L} | grep "Listener Log File" | awk '{print $NF}' | sed -r 's;[a-z0-9_\-]+\.log$;;g' | sed 's/alert.*$/trace\//'``echo ${L} | tr '[:upper:]' '[:lower:]'`".log"
+        LSRN_LOG=$(lsnrctl status ${L} | grep "Listener Log File" | awk '{print $NF}' | sed -r 's;[a-z0-9_\-]+\.log$;;g' | sed 's/alert.*$/trace\//'``echo ${L} | tr '[:upper:]' '[:lower:]')".log"
         echo $LSRN_LOG    >>  ${OUT}
 done
         cat << !                        >> ${OUT}
@@ -112,7 +112,7 @@ done
         compress
         copytruncate
         nodelaycompress
-        create 0640 `whoami` dba
+        create 0640 $(whoami) dba
         notifempty
         dateext
 }
