ef7cdf8919cbf797bfe44eb0fc749294fb824f9d
Lucas Pimentel Lellis <lucaslellis@gmail.com>
2023-02-28 10:01:13 -0300

    Alterações para o AIX

--- limpa_logs_traces_audits.sh
+++ limpa_logs_traces_audits.sh
@@ -3,7 +3,9 @@
 ##################################################################################
 ## Script: limpa_logs_traces_audits.sh
 ## Descricao: Limpa logs, traces, audits, incidentes e core dumps
-##            de todos os ORACLE_HOMEs de um servidor.
+##            de todos os ORACLE_HOMEs de um usuario.
+##            Caso haja multiplos owners de ORACLE_HOMEs, e necessario
+##            agendar a rotina na crontab de cada usuario.
 ##
 ## Autor: lucaslellis [at] gmail [dot] com
 ##
@@ -57,13 +59,13 @@ ENDEND
         dir_audit=$(\sqlplus -S "/ as sysdba" @"${SCRIPT_LIMPEZA_AUDIT}")
         echo "Diretorio: ${dir_audit}"
         # o primeiro metodo e mais rapido, mas nao funciona em todos os ambientes
+        # a saida de erros e ignorada por conta de erros de arquivo nao encontrado quando o arquivo nao existe
         find "${dir_audit}" -name '*.aud' -mtime +${DIAS_RETENCAO_AUDIT} -delete 2>/dev/null
-        find "${dir_audit}" -name '*.aud' -mtime +${DIAS_RETENCAO_AUDIT} -exec rm {} +
+        find "${dir_audit}" -name '*.aud' -mtime +${DIAS_RETENCAO_AUDIT} -exec rm {} + 2>/dev/null
         find "${dir_audit}" -name 'audit_*.zip' -mtime +${DIAS_RETENCAO_AUDIT} -delete 2>/dev/null
-        find "${dir_audit}" -name 'audit_*.zip' -mtime +${DIAS_RETENCAO_AUDIT} -exec rm {} +
+        find "${dir_audit}" -name 'audit_*.zip' -mtime +${DIAS_RETENCAO_AUDIT} -exec rm {} + 2>/dev/null
 
         # Gera zip com arquivos *.aud ainda dentro da retencao e remove os arquivos depois que o zip e completado
-        # ( cd "${dir_audit}" || exit; find . -name "*.aud" | zip --grow --quiet --move "${dir_audit}"/audit_"${DT_EXEC}".zip -@ )
         ( cd "${dir_audit}" || exit; find . -name "*.aud" -exec zip --grow --quiet --move "${dir_audit}"/audit_"${DT_EXEC}".zip {} + )
     done
 }
@@ -90,10 +92,11 @@ ENDEND
         for dir in $(cat "${SCRIPT_LIMPEZA_TRACES}.out"); do
             echo "Diretorio: ${dir}"
             # o primeiro metodo e mais rapido, mas nao funciona em todos os ambientes
+            # a saida de erros e ignorada por conta de erros de arquivo nao encontrado quando o arquivo nao existe
             find "${dir}" -name '*.trc' -mtime +${DIAS_RETENCAO_TRACES} -delete 2>/dev/null
             find "${dir}" -name '*.trm' -mtime +${DIAS_RETENCAO_TRACES} -delete 2>/dev/null
-            find "${dir}" -name '*.trc' -mtime +${DIAS_RETENCAO_TRACES} -exec rm {} +
-            find "${dir}" -name '*.trm' -mtime +${DIAS_RETENCAO_TRACES} -exec rm {} +
+            find "${dir}" -name '*.trc' -mtime +${DIAS_RETENCAO_TRACES} -exec rm {} + 2>/dev/null
+            find "${dir}" -name '*.trm' -mtime +${DIAS_RETENCAO_TRACES} -exec rm {} + 2>/dev/null
         done
         rm -f "${SCRIPT_LIMPEZA_TRACES}".out
     done
@@ -111,7 +114,7 @@ ENDEND
         echo "Instancia: ${inst}"
         . oraenv <<< "$inst" > /dev/null 2>&1
         if [ -x "$(command -v adrci)" ]; then
-            for adrci_home in $(\adrci exec="show homes" | tail -n +2); do
+            for adrci_home in $(\adrci exec="show homes" | tail -n +2 | grep -v user_root); do
                 echo "adrci_home: ${adrci_home}"
                 adrci exec="set home ${adrci_home}; migrate schema; purge -age ${retencao_adrci_min}"
             done
@@ -124,11 +127,11 @@ ENDEND
         . oraenv <<< "$inst" > /dev/null 2>&1
         if [ -x "$(command -v adrci)" ]; then
             # Clusterware 11.2
-            for adrci_home in $(\adrci exec="set base $ORACLE_HOME/log; show homes" | tail -n +2); do
+            for adrci_home in $(\adrci exec="set base $ORACLE_HOME/log; show homes" | tail -n +2 | grep -v user_root); do
                 echo "adrci_home: ${adrci_home}"
                 adrci exec="set base $ORACLE_HOME/log; set home ${adrci_home}; migrate schema; purge -age ${retencao_adrci_min}"
             done
-            for adrci_home in $(\adrci exec="show homes" | tail -n +2); do
+            for adrci_home in $(\adrci exec="show homes" | tail -n +2 | grep -v user_root); do
                 echo "adrci_home: ${adrci_home}"
                 adrci exec="set home ${adrci_home}; migrate schema; purge -age ${retencao_adrci_min}"
             done
